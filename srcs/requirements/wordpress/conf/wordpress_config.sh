f94f549301acbffe7e14b1a778395aee3d5df00c
Archips <athirion@e1r9p20.clusters.42paris.fr>
2023-03-10 09:41:21 +0100

    wordpress seems to be ok

---
+++ srcs/requirements/wordpress/conf/wordpress_config.sh
@@ -0,0 +1,36 @@
+sleep 10
+
+if [ ! -f /var/wwww/wordpress/wp-config.php ];
+
+then 
+	wp config create --allow-root \
+	       --dbname=$SQL_DATABASE \
+	       --dbuser=$SQL_USER \
+	       --dbpass=$SQL_PASSWORD \
+	       --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+	wp core install --url=$URL_SITE \
+		--title=$TITLE_SITE \
+		--admin_user=$ADMIN_NAME \
+		--admin_password=$ADMIN_PASSWORD \
+		--admin_email=$ADMIN_MAIL \
+		--path='/var/www/wordpress' \
+		--allow-root
+
+	wp user create --allow-root \
+		--role=author \
+		--user-login=$USER_NAME \
+		--user-email=$USER_MAIL \
+		--user-pass=$USER_PASS \
+		--path='/var/www/wordpress'
+
+fi
+
+if [ ! -d /run/php ];
+
+then
+	mkdir /run/php;
+
+fi
+
+/usr/sbin/php-fpm7.3 -F
