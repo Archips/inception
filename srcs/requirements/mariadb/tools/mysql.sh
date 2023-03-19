1ea20bd9d34c247ec99fb4b4171acf983dd33cab
Archips <athirion@e1r11p23.clusters.42paris.fr>
2023-03-19 13:39:53 +0100

    clean, ready to submit

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -8,11 +8,13 @@ mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
-
 mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 
+mysql -e "DELETE FROM mysql.user WHERE user=''"
+
 mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root password $SQL_ROOT_PASSWORD
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
