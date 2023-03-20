6af8a73077edd5205707d2fccc07815f68e8b047
Archibald THIRION <athirion@e1r11p23.clusters.42paris.fr>
2023-03-20 15:33:44 +0100

    fix mariadb script

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -4,6 +4,8 @@ service mysql start;
 
 sleep 3
 
+mysqladmin -u root password $SQL_ROOT_PASSWORD
+
 mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
@@ -13,8 +15,6 @@ mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED 
 mysql -e "DELETE FROM mysql.user WHERE user=''"
 
 mysql -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root password $SQL_ROOT_PASSWORD
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
